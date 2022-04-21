4c7e48ea2aab6c0c16959125c436cc27664b1bc5
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-04-22 03:02:20 +0600

    Created  Navbar styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,78 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingBasketOutlined } from "@material-ui/icons";
 import styled from "styled-components";
+
 const Component = styled.div`
   height: 60px;
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Input = styled.input`
+  border: none;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 const Navbar = () => {
   return (
     <Component>
-      <Wrapper>navbar</Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: "15px" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>E-SHOP</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingBasketOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Component>
   );
 };
