f4222c6d16cb2b4a1b24dec344946855461703c4
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-04-23 02:07:06 +0600

    Created Slider component and styled it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
