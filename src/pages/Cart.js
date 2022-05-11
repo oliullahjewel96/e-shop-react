72fd1851a961f600807003813eb2c5ea4ff5f898
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-05-11 23:13:17 +0600

    implemented cart component design

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -39,11 +40,66 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
 `;
 
 const Info = styled.div`
   flex: 3;
+`;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+const Hr = styled.hr`
+  background-color: #e6e6e6;
+  height: 1px;
+  border: none;
+  margin-bottom: 20px;
+`;
+const ProductDetails = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 110px;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 300;
 `;
 const Summary = styled.div`
   flex: 1;
@@ -65,7 +121,59 @@ const Cart = () => {
           <TopButton type="filled"> CHECKOUT NOW </TopButton>
         </Top>
         <Bottom>
-          <Info>info</Info>
+          <Info>
+            <Product>
+              <ProductDetails>
+                <Image src="https://images.pexels.com/photos/1670766/pexels-photo-1670766.jpeg?cs=srgb&dl=pexels-mnz-1670766.jpg&fm=jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOE
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 38765374887
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5{" "}
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$100</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetails>
+                <Image src="https://images.pexels.com/photos/1670766/pexels-photo-1670766.jpeg?cs=srgb&dl=pexels-mnz-1670766.jpg&fm=jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOE
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 38765374887
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5{" "}
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$100</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
           <Summary>summary</Summary>
         </Bottom>
       </Wrapper>{" "}
